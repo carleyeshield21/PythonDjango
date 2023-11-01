3d43de08cbfca2c106a96467fbc68e5f813b9ee6
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 12:49:34 +0800

    kumite

--- apply_ka_work/forms.py
+++ apply_ka_work/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
-class AplikasyonForm(forms.Form): #model for AplikasyonForm
-    firstname = forms.CharField(max_length=80)
+class AplikasyonForm(forms.Form): #model for AplikasyonForm, pattern copied from models.py
+    firstname = models.CharField(max_length=80)
+    lastname = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    trabaho = models.CharField(max_length=80)
