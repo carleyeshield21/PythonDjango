0ba45480f1545163d359d22103b5620a8438613f
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 12:44:19 +0800

    kumite

--- apply_ka_work/views.py
+++ apply_ka_work/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from forms import AplikasyonForm
 
 # Create your views here.
 def hindex(requester):
     if requester.method == 'POST':
-        
+        form = AplikasyonForm()
     return render(requester,'index.html')
     # return render(requester,'hindex.html')
