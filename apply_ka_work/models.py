7e45147896659b3378f262d24af5dc37f20a1154
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 09:50:23 +0800

    kumite

--- apply_ka_work/models.py
+++ apply_ka_work/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Forma(models.Model): #designing a database model
-    firstname = models.CharField
+    firstname = models.CharField(max_length=80)
+    lastname = models.CharField(max_length=80)
