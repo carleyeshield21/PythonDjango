e1ec695fb9c3f5eb4ea27964a538af3d72546952
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 09:52:33 +0800

    kumite

--- apply_ka_work/models.py
+++ apply_ka_work/models.py
@@ -5,3 +5,5 @@ class Forma(models.Model): #designing a database model
     firstname = models.CharField(max_length=80)
     lastname = models.CharField(max_length=80)
     email = models.EmailField()
+    date = models.DateField()
+    trabaho = models.CharField(max_length=80)
