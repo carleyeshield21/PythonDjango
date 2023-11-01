dfb49a441309b176edd4f36998343be5b5d5d0df
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 13:13:04 +0800

    kumite

--- apply_ka_work/views.py
+++ apply_ka_work/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from forms import AplikasyonForm #this module must be created inside the apply_ka_work folder inside the forms.py file
+from .forms import AplikasyonForm #this module must be created inside the apply_ka_work folder inside the forms.py file, we have to add the dot at
+# the beginning of the forms module because we are importing the forms in the same folder directory
 
 # Create your views here.
 def hindex(requester): #this function is used to retrieve the input that the user will provide in the page
@@ -10,5 +11,6 @@ def hindex(requester): #this function is used to retrieve the input that the use
             lastname = form.cleaned_data['lust_name'] #argument should be the variable in the index.html name of the input
             email = form.cleaned_data['email'] #argument should be the variable in the index.html name of the input
             occupation = form.cleaned_data['occupation'] #argument should be the variable in the index.html name of the input
+            print(firstname, lastname, email, occupation)
     return render(requester,'index.html')
     # return render(requester,'hindex.html')
