7364a39cee4b80099bc572da777111e722438fa0
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 12:58:21 +0800

    kumite

--- apply_ka_work/views.py
+++ apply_ka_work/views.py
@@ -5,5 +5,8 @@ from forms import AplikasyonForm #this module must be created inside the apply_k
 def hindex(requester):
     if requester.method == 'POST':
         form = AplikasyonForm()
+        firstname = form.cleaned_data['first_name'] #argument should be the variable in the index.html name of the input
+        lastname = form.cleaned_data['lust_name'] #argument should be the variable in the index.html name of the input
+        email = form.cleaned_data['email'] #argument should be the variable in the index.html name of the input
     return render(requester,'index.html')
     # return render(requester,'hindex.html')
