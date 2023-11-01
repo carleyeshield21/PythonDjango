19ca27f802ccf1480eba0fc406948168513276f9
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 13:29:39 +0800

    kumite

--- apply_ka_work/views.py
+++ apply_ka_work/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import AplikasyonForm #this module must be created inside the apply_ka_work folder inside the forms.py file, we have to add the dot at
 # the beginning of the forms module because we are importing the forms in the same folder directory
+from .models import Forma
 
 # Create your views here.
 def hindex(requester): #this function is used to retrieve the input that the user will provide in the page
