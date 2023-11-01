7e793bf40203cc5d8d8161ad3dfba4dfbbbe450c
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 12:40:36 +0800

    kumite

--- apply_ka_work/views.py
+++ apply_ka_work/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def hindex(requester):
-    # return render(requester,'hindex.html')
+    if requester.method == 'POST':
+        
     return render(requester,'index.html')
+    # return render(requester,'hindex.html')
