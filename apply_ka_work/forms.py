02074e4d46e3da273cd11d62ef4339f60d0bdc95
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-01 12:50:54 +0800

    kumite

--- apply_ka_work/forms.py
+++ apply_ka_work/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class AplikasyonForm(forms.Form): #model for AplikasyonForm, pattern copied from forms.py, then change forms to forms
+class AplikasyonForm(forms.Form): #model for AplikasyonForm, pattern copied from forms.py, then change models to forms
     firstname = forms.CharField(max_length=80)
     lastname = forms.CharField(max_length=80)
     email = forms.EmailField()
